65125a55056d40ae918ce94d2d1101b11d831330
Piotr Bielski <piotrbielski3@gmail.com>
2024-02-19 23:46:31 +0000

    task template view

--- taskmgmt/views.py
+++ taskmgmt/views.py
@@ -72,3 +72,16 @@ def save_user_info(request):
         "partials/user_info_read.html",
         user_info,
     )
+
+def task_template_view(req):
+    print("template().load")
+
+    return render(
+        req,
+        "partials/task_template.html"
+    )
+
+def task_list_view(req):
+    print("task_list_view().load")
+
+    return render(req, "partials/main.html")
